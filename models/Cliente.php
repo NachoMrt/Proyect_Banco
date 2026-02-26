6b81d3d0684b848f7e72e74a44c717e0f429a06a
NachoMrt <josigmcs@gmail.com>
2026-02-26 09:20:38 +0100

    Primer commit del proyecto

---
+++ models/Cliente.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once './config/conexion.php';
+
+class Cliente
+{
+    private $db;
+    public function __construct()
+    {
+        $this->db = Database::conectar();
+    }
+    public function getAll()
+    {
+        return $this->db->query("SELECT * FROM clientes")->fetchAll();
+    }
+    public function getByEmail($email)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM clientes WHERE email = ?");
+        $stmt->execute([$email]);
+        $cliente_buscar = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (filter_var($cliente_buscar, FILTER_VALIDATE_BOOLEAN) === false && $cliente_buscar === false) {
+            return false;
+        } else if (count($cliente_buscar) > 0) {
+            return $cliente_buscar;
+        }
+    }
+    public function save($nombre, $apellidos, $dni, $telefono, $email, $contrasenia)
+    {
+        $stmt = $this->db->prepare("INSERT INTO clientes (nombre, apellidos, DNI, telefono, email, password) VALUES(?,?,?,?, ?)");
+        $stmt->execute([$nombre, $apellidos, $dni, $telefono, $email, $contrasenia]);
+    }
+    public function update($id, $nombre, $apellidos, $email, $telefono)
+    {
+        $stmt = $this->db->prepare("UPDATE clientes SET nombre = ?,apellidos = ?,email = ?, telefono = ? WHERE id = ?");
+        $stmt->execute([$nombre, $apellidos, $email, $telefono, $id]);
+    }
+    public function delete($id)
+    {
+        $stmt = $this->db->prepare("DELETE FROM clientes WHERE id= ?");
+        $stmt->execute([$id]);
+    }
+
+    public function getById($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM clientes WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+
+    public function restaDinero($id_cliente, $cantidad)
+    {
+        $stmt = $this->db->prepare('UPDATE clientes SET saldo = saldo - ? WHERE id = ?');
+        $stmt->execute([$cantidad, $id_cliente]);
+    }
+}
