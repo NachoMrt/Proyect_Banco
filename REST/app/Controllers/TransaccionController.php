949cb13d2857172a294f40852db4be1359c024dd
NachoMrt <josigmcs@gmail.com>
2026-02-27 10:56:12 +0100

    creado controlador y modelo de transaccion

---
+++ REST/app/Controllers/TransaccionController.php
@@ -0,0 +1,56 @@
+<?php
+// El controlador es el que contiene la lógica real de los endpoints (CRUD, validaciones…).
+
+require_once __DIR__ . '/../Models/Transaccion.php';
+
+class ClienteController {
+
+    public function index() {
+        echo json_encode(Transaccion::all());
+    }
+
+    public function show($id) {
+        $Transaccion = Transaccion::find($id);
+        if(!$Transaccion) {
+            http_response_code(404);
+            echo json_encode(['mensaje'=>'Transaccion no encontrado']);
+        } else {
+            echo json_encode($Transaccion);
+        }
+    }
+
+    public function store($data) {
+        if(!isset($data['nombre']) || !isset($data['email'])) {
+            http_response_code(400);
+            echo json_encode(['mensaje'=>'Datos incompletos']);
+            return;
+        }
+        $Transaccion = Transaccion::create($data);
+        http_response_code(201);
+        echo json_encode($Transaccion);
+    }
+
+    public function update($id, $data) {
+        $Transaccion = Transaccion::update($id, $data);
+        if(!$Transaccion) {
+            http_response_code(404);
+            echo json_encode(['mensaje'=>'Transaccion no encontrado']);
+        } else {
+            echo json_encode($Transaccion);
+        }
+    }
+
+    public function delete($id) {
+        $result = Transaccion::delete($id);
+        if($result) {
+            echo json_encode(['mensaje'=>'Transaccion eliminado']);
+        } else {
+            http_response_code(404);
+            echo json_encode(['mensaje'=>'Transaccion no encontrado']);
+        }
+    }
+
+}
+
+
+
